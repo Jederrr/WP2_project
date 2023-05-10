44b721f909caf196bbd6a8a8b92a1cb3b1b38ebd
Jederrr <128010171+Jederrr@users.noreply.github.com>
2023-05-10 12:11:30 +0700

    Update Contoh1.php

--- Contoh1.php
+++ Contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
         public function index()
         {
             echo "<h1>Perkenalkan</h1>";
-            echo"Nama saya Iksan Maulana
-            Saya tinggal di daerah Sukabumi
-            Olah raga yang saya sukai adalah Berenang";
+            echo"Nama saya Derry Subakti Ilham
+            Saya tinggal di daerah Depok
+            Olah raga yang saya sukai adalah Mancing";
         }
 }
