bd7a98f2688d2b3a1387e42973e6bfecd836984a
Jederrr <ilhamderry111@gmail.com>
2023-05-10 12:05:24 +0700

    tugas

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+        public function index()
+        {
+            echo "<h1>Perkenalkan</h1>";
+            echo"Nama saya Iksan Maulana
+            Saya tinggal di daerah Sukabumi
+            Olah raga yang saya sukai adalah Berenang";
+        }
+}
