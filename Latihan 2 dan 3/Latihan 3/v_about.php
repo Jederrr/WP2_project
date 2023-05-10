a0a0f7b2a62b06d7826b8c62debdbab3a210cba9
Jederrr <128010171+Jederrr@users.noreply.github.com>
2023-05-10 12:13:44 +0700

    Update v_about.php

--- Latihan 2 dan 3/Latihan 3/v_about.php
+++ Latihan 2 dan 3/Latihan 3/v_about.php
@@ -2,22 +2,22 @@
  <h1><?php echo $judul ?></h1>
  <h4>Nama</h4>
  <ul type="disc">
- <li>Nama Depan : Iksan</li>
- <li>Nama Belakang : Maulana</li>
+ <li>Nama Depan : Derry</li>
+ <li>Nama Belakang : Subakti</li>
  </ul>
  <br>
  <h4>Alamat</h4>
  <ul type="none">
- <li> Sukabumi, Cicurug, KP. Pamoyanan</li>
+ <li> Depok,Cilodong , KP. Bendungan</li>
  </ul>
 
  <h4>Tempat Lahir</h4>
  <ul type="none">
- <li>Sukabumi</li>
+ <li>Depok</li>
  </ul>
  <h4>Olah Raga Favorit</h4>
  <ul type="square">
- <li>Berenang</li>
+ <li>Mancing</li>
  <li>Catur</li>
  </ul>
 </section>
